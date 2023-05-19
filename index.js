e28c93254bd2cdb318bead0f2f7490b62e1466b6
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-05-19 23:41:05 +0600

    express cors install and use

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 3000
+
+// use middlewire
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Kidoi server is running!!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
