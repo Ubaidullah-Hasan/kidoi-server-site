376e83b24a64b6e1316adcfbc5ea03184770e873
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-05-20 00:38:33 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors")
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -12,7 +13,7 @@ app.use(express.json())
 MONGODB CODE 
 *************************************/
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.clipjzr.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.clipjzr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
